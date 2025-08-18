6017029dff825f1a61d72a8eb017c1a344c909d1
pruebaes82-ai <pruebaes82@gmail.com>
2025-08-17 21:37:20 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST ?? 'localhost',
-  user: process.env.MYSQLUSER ?? 'root',
-  database: process.env.MYSQLDATABASE ?? 'easyshop',
-  password: process.env.MYSQLPASSWORD ?? '',
-  port: process.env.MYSQLPORT ?? 3306,
-  waitForConnections: true
-  
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = pool;
