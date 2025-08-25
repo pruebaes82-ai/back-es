222d45bf208bf11c3e4178a3b8d42fad599bb5f1
pruebaes82-ai <pruebaes82@gmail.com>
2025-08-24 23:50:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 //const path = require('path');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const controllers = require('./controllers/controller');
+const controllers = require('./controllers');
+const jwt = require('jsonwebtoken');
 
 
 const app = express();
@@ -19,20 +20,42 @@ app.use(cors({
 
 app.use(express.json()); // Middleware para parsear JSON
 app.use(cookieParser()); // Middleware para parsear cookies
-// Backend (Express con cors)
 
-const server = app.listen(3000, () => {
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.get('/showDatabase', controllers.showDatabase);
 
-app.get('/isLogged', controllers.isLogged);
+// Middleware para verificar el token en cada solicitud
+app.use((req, res, next) => {
+    const token = req.cookies.token
+
+    if (!token) {
+        req.user = null; // No hay token, usuario no autenticado
+        return next();  // Sigue con el routeo
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Token inválido' });
+        }
+        
+        req.user = decoded;
+        next(); // Sigue con el routeo
+    });
+});
+
 
 app.post('/register', controllers.register);
 
 app.post('/login', controllers.login);
 
+app.get('/isLogged', controllers.isLogged);
+
+app.post('/createProduct', controllers.createProduct);
+
 app.post('/purchase', controllers.purchase);
+
+app.get('/showDatabase', controllers.showDatabase);
 
 app.post('/injectSQL', controllers.injectSQL);
