b5118a8dcfab2853b804adb51b021728c44dc8e7
erikdaniel949 <danielerik949@gmail.com>
2025-10-27 10:41:14 -0300

    Refactor app.js to use ES6 module syntax

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
-const express = require('express');
-//const path = require('path');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const controllers = require('./controllers');
-const jwt = require('jsonwebtoken');
-
+import express from 'express';
+// import path from 'path';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import * as controllers from './controllers/index.js';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
-//app.use(express.static(path.join(__dirname, "public"))); // Servir archivos estáticos desde la carpeta "public"
+// app.use(express.static(path.join(process.cwd(), "public"))); // Servir archivos estáticos desde "public"
 
-
-// habilitar cors a github pages 
+// Habilitar CORS a GitHub Pages
 app.use(cors({
     origin: 'https://erikdaniel949.github.io',
-    credentials: true, // Permitir cookies
+    credentials: true,
 }));
-
 
 app.use(express.json()); // Middleware para parsear JSON
 app.use(cookieParser()); // Middleware para parsear cookies
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-
 // Middleware para verificar el token en cada solicitud
 app.use((req, res, next) => {
-    const token = req.cookies.token
+    const token = req.cookies?.token;
 
     if (!token) {
         req.user = null; // No hay token, usuario no autenticado
@@ -36,30 +28,27 @@ app.use((req, res, next) => {
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ message: 'Token inválido' });
-        }
-        
+        if (err) return res.status(403).json({ message: 'Token inválido' });
         req.user = decoded;
-        next(); // Sigue con el routeo
+        next();
     });
 });
 
-
+// Rutas
 app.post('/register', controllers.register);
-
 app.post('/login', controllers.login);
-
 app.get('/isLogged', controllers.isLogged);
 
 app.post('/createProduct', controllers.createProduct);
-
 app.post('/purchase', controllers.purchase);
 
 app.get('/getUsers', controllers.getUsers);
-
 app.get('/getProducts', controllers.getProducts);
-
 app.get('/getPurchases', controllers.getPurchases);
 
 app.post('/injectSQL', controllers.injectSQL);
+
+// Iniciar servidor
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
